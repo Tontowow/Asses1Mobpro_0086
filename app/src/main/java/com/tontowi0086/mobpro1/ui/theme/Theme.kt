53606d956c49235c233b5dd89caa89835be88772
Mochammad Tontowi Jauhari <tontowijauhari285@gmail.com>
2025-05-09 15:58:38 +0700

    menambahkan beberapa string untuk aplikasi

--- app/src/main/java/com/tontowi0086/mobpro1/ui/theme/Theme.kt
+++ app/src/main/java/com/tontowi0086/mobpro1/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.tontowi0086.mobpro1.ui.theme
+package com.tontowi0086.mobpro1.ui.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/tontowi0086/mobpro1/ui/theme/Type.kt
+++ app/src/main/java/com/tontowi0086/mobpro1/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.tontowi0086.mobpro1.ui.theme
+package com.tontowi0086.mobpro1.ui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
