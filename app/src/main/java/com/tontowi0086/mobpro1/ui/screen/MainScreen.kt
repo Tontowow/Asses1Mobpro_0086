6d321703142a14c105b898614bc69c7fc21196c2
Mochammad Tontowi Jauhari <tontowijauhari285@gmail.com>
2025-05-09 16:01:26 +0700

    membuat TopAppBar menjadi ada 2 button untuk pengaplikasian

--- app/src/main/java/com/tontowi0086/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/tontowi0086/mobpro1/ui/screen/MainScreen.kt
@@ -1,83 +1,192 @@
 package com.tontowi0086.mobpro1.ui.screen
 
-import androidx.compose.foundation.layout.*
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.tontowi0086.mobpro1.R
+import com.tontowi0086.mobpro1.model.MainViewModel
+import com.tontowi0086.mobpro1.model.Peminjaman
+import com.tontowi0086.mobpro1.navigation.Screen
+import com.tontowi0086.mobpro1.ui.ui.theme.Mobpro1Theme
+import com.tontowi0086.mobpro1.util.SettingsDataStore
+import com.tontowi0086.mobpro1.util.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class TaskViewModel : ViewModel() {
-    var tasks by mutableStateOf(listOf<String>())
-        private set
-
-    fun addTask(task: String) {
-        if (task.isNotEmpty()) {
-            tasks = tasks + task
-        }
-    }
-
-    fun editTask(index: Int, newTask: String) {
-        if (newTask.isNotEmpty()) {
-            tasks = tasks.toMutableList().also { it[index] = newTask }
-        }
-    }
-
-    fun removeTask(index: Int) {
-        tasks = tasks.toMutableList().also { it.removeAt(index) }
-    }
-}
+val colorOptions = listOf(Color(0xFFB3E5FC), Color(0xFFC8E6C9), Color(0xFFFFCDD2)) // Biru Muda, Hijau Muda, Merah Muda
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(navController: NavHostController, taskViewModel: TaskViewModel = viewModel()) {
-    var taskInput by remember { mutableStateOf(TextFieldValue()) }
+fun MainScreen(navController: NavController) {
+    val context = LocalContext.current
+    val dataStore = SettingsDataStore(context)
+    val showList by dataStore.layoutFlow.collectAsState(initial = true)
+    val selectedColorIndex by dataStore.colorIndexFlow.collectAsState(initial = 0)
+    val backgroundColor = colorOptions.getOrElse(selectedColorIndex) { Color.White }
+
+    var expanded by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "To-Do List") },
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = backgroundColor,
+                    titleContentColor = Color.Black,
+                ),
                 actions = {
-                    IconButton(onClick = { navController.navigate("about") }) {
-                        Icon(Icons.Outlined.Info, contentDescription = "Tentang")
+                    IconButton(onClick = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            dataStore.saveLayout(!showList)
+                        }
+                    }) {
+                        Icon(
+                            painter = painterResource(
+                                if (showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_view_list_24
+                            ),
+                            contentDescription = stringResource(
+                                if (showList) R.string.grid else R.string.list
+                            ),
+                            tint = Color.Black
+                        )
+                    }
+                    Box {
+                        IconButton(onClick = { expanded = true }) {
+                            Icon(
+                                imageVector = Icons.Default.MoreVert,
+                                contentDescription = "Menu",
+                                tint = Color.Black
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            colorOptions.forEachIndexed { index, _ ->
+                                DropdownMenuItem(
+                                    text = { Text("Tema Warna ${index + 1}") },
+                                    onClick = {
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            dataStore.saveColorIndex(index)
+                                        }
+                                        expanded = false
+                                    }
+                                )
+                            }
+                        }
                     }
                 }
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                taskViewModel.addTask(taskInput.text.trim())
-                taskInput = TextFieldValue()
-            }) {
-                Text("+")
+            FloatingActionButton(
+                onClick = { navController.navigate(Screen.FormBaru.route) },
+                containerColor = backgroundColor
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.tambah_catatan),
+                    tint = Color.Black
+                )
             }
-        }
-    ) { paddingValues ->
+        },
+        containerColor = backgroundColor
+    ) { innerPadding ->
+        ScreenContent(showList, Modifier.padding(innerPadding), navController)
+    }
+}
+
+@Composable
+fun ScreenContent(showList: Boolean, modifier: Modifier = Modifier, navController: NavController) {
+    val context = LocalContext.current
+    val factory = ViewModelFactory(context)
+    val viewModel: MainViewModel = viewModel(factory = factory)
+    val data by viewModel.data.collectAsState()
+
+    if (data.isEmpty()) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp)
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(
-                value = taskInput,
-                onValueChange = { taskInput = it },
-                label = { Text("Tambah Tugas") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            LazyColumn {
-                items(taskViewModel.tasks) { task ->
-                    TaskItem(taskViewModel.tasks.indexOf(task), task, taskViewModel)
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    } else {
+        if (showList) {
+            LazyColumn(
+                modifier = modifier.fillMaxSize(),
+                contentPadding = PaddingValues(bottom = 84.dp)
+            ) {
+                items(data) {
+                    ListItem(peminjaman = it) {
+                        navController.navigate(Screen.FormUbah.withId(it.id))
+                    }
+                    HorizontalDivider()
+                }
+            }
+        } else {
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxSize(),
+                columns = StaggeredGridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalItemSpacing = 8.dp,
+                contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 84.dp)
+            ) {
+                items(data) {
+                    GridItem(peminjaman = it) {
+                        navController.navigate(Screen.FormUbah.withId(it.id))
+                    }
                 }
             }
         }
@@ -85,49 +194,65 @@ fun MainScreen(navController: NavHostController, taskViewModel: TaskViewModel = 
 }
 
 @Composable
-fun TaskItem(index: Int, task: String, taskViewModel: TaskViewModel) {
-    var isEditing by remember { mutableStateOf(false) }
-    var editedTask by remember { mutableStateOf(TextFieldValue(task)) }
-
-    Card(
+fun ListItem(peminjaman: Peminjaman, onClick: () -> Unit) {
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp),
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(4.dp)
+            .clickable { onClick() }
+            .padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            if (isEditing) {
-                OutlinedTextField(
-                    value = editedTask,
-                    onValueChange = { editedTask = it },
-                    label = { Text("Edit Tugas") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Button(onClick = {
-                        taskViewModel.editTask(index, editedTask.text.trim())
-                        isEditing = false
-                    }) {
-                        Text("Simpan")
-                    }
-                    Button(onClick = { isEditing = false }) {
-                        Text("Batal")
-                    }
-                }
-            } else {
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(task, style = MaterialTheme.typography.bodyLarge)
-                    Row {
-                        IconButton(onClick = { isEditing = true }) {
-                            Icon(Icons.Outlined.Edit, contentDescription = "Edit")
-                        }
-                        IconButton(onClick = { taskViewModel.removeTask(index) }) {
-                            Icon(Icons.Outlined.Delete, contentDescription = "Hapus")
-                        }
-                    }
-                }
-            }
+        Text(
+            text = peminjaman.namaPeminjam,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = peminjaman.namaBarang,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = "${peminjaman.jumlahHari} hari")
+    }
+}
+
+@Composable
+fun GridItem(peminjaman: Peminjaman, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, DividerDefaults.color)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = peminjaman.namaPeminjam,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = peminjaman.namaBarang,
+                maxLines = 4,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(text = "${peminjaman.jumlahHari} hari")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    Mobpro1Theme {
+        MainScreen(rememberNavController())
     }
 }
