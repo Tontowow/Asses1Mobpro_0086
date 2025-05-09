fd1edabc626dd56784c3e3d24c344b399eb22ba8
Mochammad Tontowi Jauhari <tontowijauhari285@gmail.com>
2025-05-09 16:02:38 +0700

    Menavigasikan untuk form Peminjaman

--- app/src/main/java/com/tontowi0086/mobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/tontowi0086/mobpro1/navigation/NavGraph.kt
@@ -2,23 +2,36 @@ package com.tontowi0086.mobpro1.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.tontowi0086.mobpro1.ui.screen.AboutScreen
+import androidx.navigation.navArgument
+import com.tontowi0086.mobpro1.ui.screen.DetailScreen
+import com.tontowi0086.mobpro1.ui.screen.KEY_ID_PEMINJAMAN
 import com.tontowi0086.mobpro1.ui.screen.MainScreen
 
+
 @Composable
-fun SetupNavGraph(navController: NavHostController) {
+fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = Screen.Home.route
     ) {
-        composable("home") {
+        composable(route = Screen.Home.route) {
             MainScreen(navController)
         }
-        composable("about") {
-            AboutScreen(navController)
+        composable(route = Screen.FormBaru.route) {
+            DetailScreen(navController)
+        }
+        composable(
+            route = Screen.FormUbah.route,
+            arguments = listOf(
+                navArgument(KEY_ID_PEMINJAMAN) { type = NavType.LongType }
+            )
+        ) { navBackStackEntry ->
+            val id = navBackStackEntry.arguments?.getLong(KEY_ID_PEMINJAMAN)
+            DetailScreen(navController, id)
         }
     }
 }
--- app/src/main/java/com/tontowi0086/mobpro1/navigation/Screen.kt
+++ app/src/main/java/com/tontowi0086/mobpro1/navigation/Screen.kt
@@ -1,7 +1,11 @@
 package com.tontowi0086.mobpro1.navigation
 
-sealed class Screen (val route: String){
-    data object Home: Screen("mainscreen")
-    data object About: Screen("aboutscreen")
+import com.tontowi0086.mobpro1.ui.screen.KEY_ID_PEMINJAMAN
 
+sealed class Screen(val route: String){
+    data object  Home: Screen("mainScreen")
+    data object  FormBaru: Screen("detailScreen")
+    data object  FormUbah: Screen("detailScreen/{$KEY_ID_PEMINJAMAN}"){
+        fun withId(id: Long) = "detailScreen/$id"
+    }
 }
